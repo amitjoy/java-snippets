c351d9a4666075a46cd59cf4a6f958c17ca60ce3
Amit Kumar Mondal <admin@amitinside.com>
2016-01-10 00:38:01 +0100

    Added More Reified Generics Examples

--- src/benblack86/java/Reification.java
+++ src/benblack86/java/Reification.java
@@ -1,32 +1,14 @@
 package benblack86.java;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class Reification {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Collection<String> collection = Arrays.asList("hi", "there");
-		List<String> list = Reification.asList(collection);
-		System.out.println(list);
-		
-		List<Object> objects = Arrays.<Object>asList("three", "four");
-		List<String> strings = Reification.promote(objects);
-		System.out.println(strings);
-		
-		String[] stringArray = Reification.toArray(strings, new String[0]);
-		System.out.println(Arrays.toString(stringArray));
-		
-		String[] stringArray2 = Reification.toArray(strings, String.class);
-		System.out.println(Arrays.toString(stringArray2));
-	}
-
-	public static <T> List<T> asList(Collection<T> collection) {
+	public static <T> List<T> asList(final Collection<T> collection) {
 		// cannot do List<T> as information is erased
 		if (collection instanceof List<?>) {
 			// can cast using T as the argument expects T
@@ -35,41 +17,73 @@ public class Reification {
 			throw new IllegalArgumentException("Argument not a list");
 		}
 	}
-	
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final Collection<String> collection = Arrays.asList("hi", "there");
+		final List<String> list = Reification.asList(collection);
+		System.out.println(list);
+
+		final List<Object> objects = Arrays.<Object> asList("three", "four");
+		final List<String> strings = Reification.promote(objects);
+		System.out.println(strings);
+
+		final String[] stringArray = Reification.toArray(strings, new String[0]);
+		System.out.println(Arrays.toString(stringArray));
+
+		final String[] stringArray2 = Reification.toArray(strings, String.class);
+		System.out.println(Arrays.toString(stringArray2));
+
+		final List<String> strins = new ArrayList<String>() { // Type Erasure
+																// will not be
+																// performed
+			@Override
+			public void clear() {
+				// TODO Auto-generated method stub
+				super.clear();
+			}
+		};
+		System.out.println(strins);
+	}
+
 	@SuppressWarnings("unchecked")
-	public static List<String> promote(List<Object> objs) {
-		for (Object obj : objs) {
+	public static List<String> promote(final List<Object> objs) {
+		for (final Object obj : objs) {
 			if (!(obj instanceof String)) {
 				throw new ClassCastException();
 			}
 		}
-		
-		return (List<String>)(List<?>)objs; // unchecked cast
+
+		return (List<String>) (List<?>) objs; // unchecked cast
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static <T> T[] toArray(Collection<T> collection, T[] a) {
-		if (a.length < collection.size()) {
-			a = (T[])Array.newInstance(a.getClass().getComponentType(), collection.size()); // unchecked cast
-		}
-		
+	public static <T> T[] toArray(final Collection<T> collection, final Class<T> c) {
+		final T[] a = (T[]) Array.newInstance(c, collection.size()); // unchecked
+																		// cast
+
 		int i = 0;
-		for(T element : collection) {
+		for (final T element : collection) {
 			a[i++] = element;
 		}
-		
+
 		return a;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static <T> T[] toArray(Collection<T> collection, Class<T> c) {
-		T[] a = (T[])Array.newInstance(c, collection.size()); // unchecked cast
-		
+	public static <T> T[] toArray(final Collection<T> collection, T[] a) {
+		if (a.length < collection.size()) {
+			a = (T[]) Array.newInstance(a.getClass().getComponentType(), collection.size()); // unchecked
+																								// cast
+		}
+
 		int i = 0;
-		for(T element : collection) {
+		for (final T element : collection) {
 			a[i++] = element;
 		}
-		
+
 		return a;
 	}
 }
